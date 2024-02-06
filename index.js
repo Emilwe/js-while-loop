d9b2cbb2be3402d5dc2f1d7f23cd9705ab09cf95
Emilwe <emilwest@viken.no>
2024-02-06 12:53:43 +0100

    school assingment

---
+++ index.js
@@ -0,0 +1,75 @@
+//opg 1
+/*
+let i=1
+while(i > 51 ) {
+    console.log(i)
+    i++
+}
+*/
+
+//opg 2
+/*
+let i = 2
+while(i < 100) {
+    console.log(i)
+    i = i + 2
+}
+*/
+
+//opg 3
+/*
+let i = 3
+while(i < 999){
+    console.log(i)
+    i = i + 3
+}
+*/
+
+//opg 4
+/*
+let i = 1
+while(i<=1000){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i)
+    }
+    i++
+
+}
+*/
+
+//opg 5
+/*
+let sum = 0
+let i = 1
+while(i <= 100){
+    sum += i
+    i++
+}
+
+console.log(sum)
+*/
+
+//opg 6
+ /*
+let linje =''
+
+let antalltegn = 1
+
+while (antalltegn <=4) {
+    linje += '#'
+    console.log(linje)
+    antalltegn++
+}
+*/
+
+//opg 7
+
+let linje =''
+
+let antalltegn = 1
+
+while (antalltegn <50) {
+    linje += '#'
+    console.log(linje)
+    antalltegn++
+}
